fd447dc273822247f18142f6bd6ea2106fd6a26c
Ramiro Berrelleza <rberrelleza@gmail.com>
2020-01-22 12:00:12 -0800

    add dev environment

--- generate.sh
+++ generate.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-set -ex
+URL=${1:-'https://apps.okteto.com'}
+
+# fail if not in helm3
+helm version | grep -q v3
+if [ $? -eq 1 ]; then 
+  echo 'Error: helm v3 is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v yq)" ]; then
+  echo 'Error: yq is not installed.' >&2
+  exit 1
+fi
+
+set -e
 
 for d in */ ; do
-    helm3 lint $d
-    helm3 package $d --destination $d
+    helm lint $d
+    helm package $d --destination $d
 done
 
-helm3 repo index --url https://apps.okteto.com .
+helm repo index --url $URL .
 yq w -s devs.yaml index.yaml > index.yaml.generated
 mv index.yaml.generated index.yaml
