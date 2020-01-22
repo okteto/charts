4e5d54624ed232d836dd6935d1440175ec8633dd
Ramiro Berrelleza <rberrelleza@gmail.com>
2020-01-22 11:18:42 -0800

    regenerate all tar.gz

--- generate.sh
+++ generate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+for d in */ ; do
+    helm3 package $d --destination $d
+done
+
 helm3 repo index --url https://apps.okteto.com .
 yq w -s devs.yaml index.yaml > index.yaml.generated
 mv index.yaml.generated index.yaml
